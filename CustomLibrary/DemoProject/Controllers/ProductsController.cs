c9506e771649cc0efa0018c742665fb706469c9a
Antônio Rocha <antoniojunior159@gmail.com>
2025-06-12 22:31:24 -0300

    Adjust Project

---
+++ CustomLibrary/DemoProject/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoFasterConnect;
+
+namespace DemoProject.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IMongoCollection<Product> _collection;
+
+    public ProductsController()
+    {
+        var mongo = new MongoDBService("mongodb://localhost:27017", "TestDb");
+        _collection = mongo.GetCollection<Product>("Products");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var all = await _collection.Find(_ => true).ToListAsync();
+        return Ok(all);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] Product product)
+    {
+        await _collection.InsertOneAsync(product);
+        return Ok();
+    }
+}
+
+
+public class Product
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
